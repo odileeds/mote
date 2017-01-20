50b35fa8af26ad0f1acb572e2e338faf7c7dc045
Stuart <gravatar@strudel.org.uk>
2017-01-20 11:56:48 +0000

    Better data format sent on reload

--- mote.js
+++ mote.js
@@ -3,13 +3,17 @@ var graph,graph2;
 
 function loadData(d,attr){
 
-	//City,2010,2011,2012,2013,2014,2015
+	// Remove extra newlines at the end
+	d = d.replace(/[\n\r]$/,"");
+
 	data = CSV2JSON(d,[{'name':'unixdate','format':'number'},
 								{'name':'date','format':'date'},
 								{'name':'T','format':'number'},
 								{'name':'L','format':'number'}
 	]);
+
 	processLoad();
+
 	return this;
 }
 
@@ -29,33 +33,32 @@ function processLoad(){
 				s2.push({x:data[i].date,y:data[i].L,date:(data[i].date).toISOString()});
 			}
 		}
+		temp.push({
+			// Data in the form [{x:x1,y:y1,err:err1},...{x:xn,y:yn,err:errn}]
+			data: s1,
+			color: '#D60303',
+			points: { show: true, radius: 3 },
+			lines: { show: false ,width: 1 },
+			title: 'LoRaWAN mote',
+			clickable: false,
+			hoverable: true,
+			// Modify the default hover text with replacements
+			hover: {
+				text: 'Temperature: <strong>{{y}}'+parseHTML('&deg;')+'C</strong><br />{{date}}',
+				before: '{{title}}<br />'
+			},
+			css: {
+			  'font-size': '0.8em',
+			  'background-color': 'white',
+			  'color': 'black',
+			  'padding': '1em',
+			  'border-radius': '0px'
+			}
+		});
 
 		if(graph){
-			graph.updateData(data);
-			graph2.updateData(data);
+			graph.updateData(temp);
 		}else{
-			temp.push({
-				// Data in the form [{x:x1,y:y1,err:err1},...{x:xn,y:yn,err:errn}]
-				data: s1,
-				color: '#D60303',
-				points: { show: true, radius: 3 },
-				lines: { show: false ,width: 1 },
-				title: 'LoRaWAN mote',
-				clickable: false,
-				hoverable: true,
-				// Modify the default hover text with replacements
-				hover: {
-					text: 'Temperature: <strong>{{y}}'+parseHTML('&deg;')+'C</strong><br />{{date}}',
-					before: '{{title}}<br />'
-				},
-				css: {
-				  'font-size': '0.8em',
-				  'background-color': 'white',
-				  'color': 'black',
-				  'padding': '1em',
-				  'border-radius': '0px'
-				}
-			})
 			S('#temperature').css({"width":"100%","height":"350px","margin-bottom":"16px"});
 			graph = $.graph('temperature', temp, {
 				xaxis: { 'label': 'Date/time', 'mode': 'time' },
@@ -64,28 +67,32 @@ function processLoad(){
 				hoverable: true,
 				grid: { hoverable: true, clickable: true, show: false, background: 'transparent', color: 'rgba(0,0,0,1)', colorZoom: 'rgba(214,3,3,0.2)' }
 			});
-		
-			light.push({
-				data: s2,
-				color: '#F9BC26',
-				points: { show: true, radius: 3 },
-				lines: { show: false ,width: 1 },
-				title: 'LoRaWAN mote',
-				clickable: false,
-				hoverable: true,
-				// Modify the default hover text with replacements
-				hover: {
-					text: 'Light: <strong>{{y}}</strong><br />{{date}}',
-					before: '{{title}}<br />'
-				},
-				css: {
-				  'font-size': '0.8em',
-				  'background-color': 'white',
-				  'color': 'black',
-				  'padding': '1em',
-				  'border-radius': '0px'
-				}
-			})
+		}
+
+		light.push({
+			data: s2,
+			color: '#F9BC26',
+			points: { show: true, radius: 3 },
+			lines: { show: false ,width: 1 },
+			title: 'LoRaWAN mote',
+			clickable: false,
+			hoverable: true,
+			// Modify the default hover text with replacements
+			hover: {
+				text: 'Light: <strong>{{y}}</strong><br />{{date}}',
+				before: '{{title}}<br />'
+			},
+			css: {
+			  'font-size': '0.8em',
+			  'background-color': 'white',
+			  'color': 'black',
+			  'padding': '1em',
+			  'border-radius': '0px'
+			}
+		})
+		if(graph2){
+			graph2.updateData(light);
+		}else{
 			S('#light').css({"width":"100%","height":"350px","margin-bottom":"16px"});
 			graph2 = $.graph('light', light, {
 				xaxis: { 'label': 'Date/time', 'mode': 'time' },
@@ -182,8 +189,8 @@ S(document).ready(function(){
 	}
 
 	// Update every 10 minutes
+	var intervalID = window.setInterval(getData, 3000);
 	getData();
-	var intervalID = window.setInterval(getData, 600000);
 
 });
 
